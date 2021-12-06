1c225a665f3713be764f0e776c0d8a49fee53003
Brian <bshin132@gmail.com>
2021-12-06 23:14:56 +0000

    added new route

--- express_server.js
+++ express_server.js
@@ -25,6 +25,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
